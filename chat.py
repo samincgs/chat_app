87d030d69a7f8aa76f5ffe86f8798da05a812551
samin <samincgs@gmail.com>
2024-08-14 14:47:26 -0400

    working on the html and css of the page

--- chat.py
+++ chat.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 channels = {'c1': ['hey!', 'hello!'], 'c2': ['eeeee', 'ooooo', 'ahhhhhh']}
 
+f = open('messaging.html', 'r')
+messaging_layout = f.read()
+f.close()
+
 @app.route('/')
 def home():
     output = 'Welcome to my home. We appear to be lacking in <i>furniture</i>.<br><br>Channels:<br>'
@@ -13,11 +17,12 @@ def home():
 
 @app.route('/channel/<channel_id>')
 def channel(channel_id):
-    output = '<a href="/">Home Page</a><br><br>'
-    for message in channels[channel_id]:
-        output += message + '<br>'
-    return output
+    return messaging_layout
+    # output = '<a href="/">Home Page</a><br><br>'
+    # for message in channels[channel_id]:
+    #     output += message + '<br>'
+    # return output
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=3000, debug=True)
     
