5c91e642ba03d9ba3d0df017aa37c2366af1c19b
samin <samincgs@gmail.com>
2024-08-14 16:34:33 -0400

    set up endpoints for the api

--- chat.py
+++ chat.py
@@ -1,12 +1,54 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
+import time
 
 app = Flask(__name__)
 
-channels = {'c1': ['hey!', 'hello!'], 'c2': ['eeeee', 'ooooo', 'ahhhhhh']}
+channels = {
+    'food': [
+    {
+        'name': 'Samincgs',
+        'msg': 'I love chocolate!',
+        'time': 0,
+    },
+    {
+        'name': 'Denise',
+        'msg': 'Me too!',
+        'time': 1,
+    },
+    {
+        'name': 'Denji',
+        'msg': 'Ugh',
+        'time': 2,
+    },
+  ],
+     'games': [
+    {
+        'name': 'Zilly',
+        'msg': 'I love vaegrant',
+        'time': 0,
+    },
+    {
+        'name': 'Brush',
+        'msg': 'It needs work.',
+        'time': 1,
+    },
+    {
+        'name': 'potato',
+        'msg': 'Its amazing!',
+        'time': 2,
+    },
+  ],
+    
+}
 
-f = open('messaging.html', 'r')
-messaging_layout = f.read()
-f.close()
+def clean(string):
+    return string.replace('<', '').replace('>', '')
+
+# option 1
+# f = open('messaging.html', 'r')
+# messaging_layout = f.read()
+# f.close()
+# return messaging_layout
 
 @app.route('/')
 def home():
@@ -17,11 +59,36 @@ def home():
 
 @app.route('/channel/<channel_id>')
 def channel(channel_id):
-    return messaging_layout
+    #option 2
+    return render_template('messaging.html')
     # output = '<a href="/">Home Page</a><br><br>'
     # for message in channels[channel_id]:
     #     output += message + '<br>'
     # return output
+
+@app.route('/api/channels')
+def api_channels():
+    return jsonify(list(channels))
+
+@app.route('/api/channel/<channel_id>')
+def api_channel(channel_id):
+    return jsonify(channels[channel_id])
+
+@app.route('/api/send_message', methods=['POST'])
+def api_send_message():
+    sent_message = request.json()
+    
+    channel = sent_message['channel']
+    
+    message_data = {
+        'msg': clean(sent_message['msg']),
+        'name': clean(sent_message['name']),
+        'time': time.time(),
+    }
+    
+    channels['channel'].append(message_data)
+    
+    return jsonify({'status': 'sucess'})
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
